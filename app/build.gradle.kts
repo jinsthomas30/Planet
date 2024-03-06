c87570bb03845ddc92ee4a45192ced0144b317e6
jinsthomas30 <jinsthomas1992@gmail.com>
2024-03-06 12:25:53 +0000

    Added Following dependencies:   # compose_navigation  = "2.7.7"   # retrofit = "2.9.0"   # loggingInterceptor = "4.5.0"   # hiltAndroid = "2.48"   # coroutines = "1.7.1"   # livedata = "2.7.0"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-parcelize")
+    kotlin("kapt")
 }
 
 android {
@@ -30,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
     buildFeatures {
         compose = true
@@ -50,20 +52,49 @@ android {
 }
 
 dependencies {
+    val navVersion = "2.7.7"
+    val retrofit = "2.9.0"
+    val loggingInterceptor = "4.5.0"
+    val hiltAndroid = "2.48"
+    val coroutines = "1.7.1"
+    val livedata = "2.7.0"
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose:1.8.2")
+
+    // Integration Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3:1.2.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:$navVersion")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:$retrofit")
+    implementation ("com.squareup.okhttp3:logging-interceptor:$loggingInterceptor")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofit")
+    implementation ("com.squareup.retrofit2:adapter-rxjava3:$retrofit")
+
+    //Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:$hiltAndroid")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltAndroid")
+
+    //Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines")
+
+    //livedata
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$livedata")
+
+
 }
